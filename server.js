f2c1dd5907661f9b5f4bcf4e832f0c277a1ee143
martin.boos <martin.boos@autexis.com>
2017-09-27 13:59:59 +0200

    files added

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+
+var PORT = 4000;
+
+// ENABLE CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get("/weather", function(req, res, next) {
+    var json = {
+        weather: 'good'
+    };
+    // res.send('{ "id": "' + req.params.id + '"}');
+    res.send(JSON.stringify(json));
+});
+
+app.get("/Weather", function(req, res, next) {
+    var json = {
+        weather: 'bad'
+    };
+    // res.send('{ "id": "' + req.params.id + '"}');
+    res.send(json);
+});
+
+app.get("/sensor/:id", function(req, res, next) {
+    var json = {
+        id: req.params.id,
+        name: 'Allgemein',
+        values: [90,88,87,95,92,85,84,20,22,21,22,22,22,19,21,30,33,45,62,59,58,72,82,85,87,90,91,90,91,92]
+    };
+    res.send(JSON.stringify(json));
+});
+
+app.listen(PORT, function () {
+    console.log(`Listening on port ${PORT}`)
+})
